d8d9289a374da8ba29af2ed3d6c690b35b597948
Roman Vlasiuk <romanvl@interlink-ua.com>
2018-07-27 14:13:30 +0300

    add django-tenants

--- kapua/settings.py
+++ kapua/settings.py
@@ -78,9 +78,13 @@ WSGI_APPLICATION = 'kapua.wsgi.application'
 
 DATABASES = {
      'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-     }
+    'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'kapua',
+            'USER': 'postgres',
+            'PASSWORD': '1254',
+            'HOST': 'localhost',
+            'PORT': '',
+         }
 }
 
 
