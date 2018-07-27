8107414d418ce08b7ccec41cbcc201db2ef6373e
Roman Vlasiuk <romanvl@interlink-ua.com>
2018-07-27 14:40:27 +0300

    add new model Product

--- testApp/models.py
+++ testApp/models.py
@@ -26,3 +26,12 @@ class Task(models.Model):
 
     def __str__(self):
         return self.title
+
+class Product(models.Model):
+    owner = models.ForeignKey('testApp.Product', on_delete=models.CASCADE, blank = True, null=True)
+    name = models.CharField(max_length=100) 
+    created_date = models.DateTimeField(
+            default=timezone.now)
+
+    def __str__(self):
+        return self.name
