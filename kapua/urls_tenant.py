7859933949f1ef3185e196d5e9d7afbbfa35bde0
Roman Vlasiuk <romanvl@interlink-ua.com>
2018-07-31 21:54:09 +0300

    add django-tenants

--- kapua/urls_tenant.py
+++ kapua/urls_tenant.py
@@ -1,15 +1,13 @@
 from django.contrib import admin
-from django.urls import path, include
 
 from django.conf.urls import url
-from testApp.views import category_list, category_entity, category_addEntity, category_moveEntity, category_deleteEntity
+from testApp.views import category_list, category_entity, category_add, category_move, category_delete
 
 urlpatterns = [
-	# path('admin/', admin.site.urls),
 	url(r'admin/', admin.site.urls),
     url(r'^category/list/$', category_list),
     url(r'^category/entity/$', category_entity),
-    url(r'^category/add/$', category_addEntity),
-    url(r'^category/move/$', category_moveEntity),
-    url(r'^category/delete/$', category_deleteEntity),    
+    url(r'^category/add/$', category_add),
+    url(r'^category/move/$', category_move),
+    url(r'^category/delete/$', category_delete),    
 ]
--- testApp/views.py
+++ testApp/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse, JsonResponse
 
 from tenant_schemas.utils import remove_www
 
-from testApp.models import Client, Category
+from testApp.models import Category
 
 def category_list(request):
     data = Category.dump_bulk()
@@ -14,26 +14,31 @@ def category_list(request):
     return JsonResponse(data, safe=False)
 
 def category_entity(request):
-    id = request.GET.get('id', '')
+    id = request.GET.get('id', 0)
 
     get = lambda node_id: Category.objects.get(pk=node_id)    
     node = get(id)
 
     return JsonResponse(node.to_json(), safe=False)
 
-def category_addEntity(request):
-    parentId = request.GET.get('parentId', '')
+def category_add(request):
+    parentId = request.GET.get('parentId', 0)
     name = request.GET.get('name', '')
 
+    # print name
+
     new_node = Category(name = name)
-    get = lambda node_id: Category.objects.get(pk=node_id)
-    node = get(parentId)
-    node.add_child(instance=new_node)
+    try:
+        get = lambda node_id: Category.objects.get(pk=node_id)   
+        node = get(parentId)
+        node.add_child(instance=new_node)
+    except DoesNotExist as e:        
+        new_node = Category.add_root(instance=new_node)
 
     return JsonResponse(new_node.to_json(), safe=False)
 
-def category_moveEntity(request):
-    parentId = request.GET.get('parentId', '')
+def category_move(request):
+    parentId = request.GET.get('parentId', 0)
     id = request.GET.get('id', '')
 
     get = lambda node_id: Category.objects.get(pk=node_id)
@@ -45,8 +50,8 @@ def category_moveEntity(request):
 
     return JsonResponse('ok', safe=False)
 
-def category_deleteEntity(request):
-    id = request.GET.get('id', '')
+def category_delete(request):
+    id = request.GET.get('id', 0)
 
     get = lambda node_id: Category.objects.get(pk=node_id)
 
